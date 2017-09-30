70a6b6bc3dbe77b01a1bc14963f627cdccdac35b
nbhuiyan <nazimudd@ualberta.ca>
2017-09-30 00:56:49 -0600

    intermediate changes

--- app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/CounterDetailsActivity.java
+++ app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/CounterDetailsActivity.java
@@ -3,11 +3,11 @@ package com.cmput301ualberta.nazim.nazimudd_countbook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CounterDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_counter_details);
     }
 }
