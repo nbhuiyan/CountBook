e6d9f278fa4ec7fd59b69c518b572240aac27468
nbhuiyan <nazimudd@ualberta.ca>
2017-09-30 01:39:27 -0600

    layout changes

--- app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/EditValueActivity.java
+++ app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/EditValueActivity.java
@@ -3,13 +3,11 @@ package com.cmput301ualberta.nazim.nazimudd_countbook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CounterDetailsActivity extends AppCompatActivity {
+public class EditValueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_counter_details);
+        setContentView(R.layout.activity_edit_value);
     }
 }
-
-
