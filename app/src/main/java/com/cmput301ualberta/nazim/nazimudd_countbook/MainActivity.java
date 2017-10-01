612ec83957159babc4873198ecd46ecd8928a374
nbhuiyan <nazimudd@ualberta.ca>
2017-09-30 23:25:41 -0600

    intermediate changes

--- app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/MainActivity.java
+++ app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/MainActivity.java
@@ -1,24 +1,34 @@
 package com.cmput301ualberta.nazim.nazimudd_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter<Counter> counterArrayAdapter;
+    private ListView counterListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void creteNewCounter(View view){
-
+        Intent intent = new Intent(this, NewCounterActivity.class);
+        startActivity(intent);
     }
 
-    public void viewCounter(View view){
-
+    public void addCounter(Counter newCounter){
+        counterArrayAdapter.add(newCounter);
     }
+
 }
 
 
--- app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/NewCounterActivity.java
+++ app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/NewCounterActivity.java
@@ -1,7 +1,10 @@
 package com.cmput301ualberta.nazim.nazimudd_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class NewCounterActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class NewCounterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_counter);
+
+        //probably dont need it
+        Intent intent = getIntent();
+
+
+    }
+
+    public void addCounter(View view){
+        EditText newName = (EditText) findViewById(R.id.newName);
+        EditText newInitVal = (EditText) findViewById(R.id.newInitVal);
+        EditText newMessage = (EditText) findViewById(R.id.newMessage);
+
+        Intent intent = getIntent();
+
     }
 }
