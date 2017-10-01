2d31d3420f0b298767daf692746d97a2fde1f054
nbhuiyan <nazimudd@ualberta.ca>
2017-09-30 21:48:58 -0600

    intermediate changes

--- app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/MainActivity.java
+++ app/src/main/java/com/cmput301ualberta/nazim/nazimudd_countbook/MainActivity.java
@@ -2,6 +2,7 @@ package com.cmput301ualberta.nazim.nazimudd_countbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void creteNewCounter(View view){
+
+    }
+
+    public void viewCounter(View view){
+
+    }
 }
+
+
